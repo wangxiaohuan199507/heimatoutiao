9c91aa9435607b4c2fcc740dceca09068bac170c
wangxiaohuan <1185693147@qq.com>
2019-12-24 10:38:04 +0800

    全局注册一个面包屑组件

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 import layoutAside from './home/layout-aside'
 import layoutHeader from './home/layout-header'
+import breadCrumb from './common/bread-crumb'
 export default {
   install (Vue) {
     Vue.component('layout-aside', layoutAside) // 注册一个全局组件
     Vue.component('layout-header', layoutHeader)
+    Vue.component('bread-crumb', breadCrumb) // 全局 注册一个面包屑组件
   }
 }
