232e1a5d61ddc29881e7a5912a139eee5fb26037
wangxiaohuan <1185693147@qq.com>
2019-12-28 10:55:15 +0800

    内容列表 页面结构，搜索工具栏

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -5,12 +5,58 @@
             文章列表
         </template>
       </bread-crumb>
+      <!-- 表单容器 -->
+      <el-form style="padding-left:50px">
+        <el-form-item label="文章列表：">
+          <!-- 放置一个单选组 文章状态：1-草稿 2-审核通过 3-审核失败 4-已删除-->
+          <el-radio-group v-model="searchForm.status">
+            <!-- label -->
+            <el-radio :label="5">全部</el-radio>
+            <el-radio :label="0">草稿</el-radio>
+            <el-radio :label="1">待审核</el-radio>
+            <el-radio :label="2">审核通过</el-radio>
+            <el-radio :label="3">审核失败</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="频道列表：">
+          <el-select placeholder="请选择频道" v-model="searchForm.channel_id">
+            <!-- el-option label是显示值 value是存储值 -->
+            <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option> -->
+          </el-select>
+        </el-form-item>
+        <el-form-item label="时间选择：">
+          <!-- 日期选择器 日期范围 -->
+          <el-date-picker v-model="searchForm.dataRange" type="daterange"></el-date-picker>
+        </el-form-item>
+      </el-form>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      searchForm: {
+        status: 5, // 默认应该选中全部
+        channel_id: null, // 默认不选中任何一个分类
+        dataRange: [] // 日期范围
+      },
+      channels: [] // 接收频道数据
+    }
+  },
+  methods: {
+    // 获取所有的频道
+    getChannels () {
+      this.$axios({
+        url: '/channels'
+      }).then(result => {
+        this.channels = result.data.channels
+      })
+    }
+  },
+  created () {
+    this.getChannels() // 获取文章数据
+  }
 }
 </script>
 
