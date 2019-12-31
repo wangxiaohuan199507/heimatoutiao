eced612fa6200f83d00b44fa66a797f9593a2e08
wangxiaohuan <1185693147@qq.com>
2019-12-31 10:41:13 +0800

    发表文章 注册富文本编辑器并使用

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-card>
+  <el-card v-loading="loading">
       <bread-crumb slot="header">
         <template slot="title">
           发布文章
@@ -11,9 +11,9 @@
           <el-input v-model="formData.title" style="width:60%"></el-input>
         </el-form-item>
         <el-form-item prop="content" label="内容">
-          <el-input v-model="formData.content" type="textarea" :rows="4"></el-input>
+          <quill-editor v-model="formData.content" style="height:300px"></quill-editor>
         </el-form-item>
-        <el-form-item prop="cover" label="封面">
+        <el-form-item style="margin-top:120px" prop="cover" label="封面">
           <el-radio-group v-model="formData.cover.type">
             <el-radio :label="1">单图</el-radio>
             <el-radio :label="3">三图</el-radio>
@@ -38,6 +38,7 @@
 export default {
   data () {
     return {
+      loading: false,
       channels: [], // 接收频道数据
       formData: {
         title: '', // 文章标题
@@ -144,9 +145,11 @@ export default {
     },
     // 通过id查询文章数据
     getArticleById (articleId) {
+      this.loading = true
       this.$axios({
         url: `/articles/${articleId}`
       }).then(result => {
+        this.loading = false
         this.formData = result.data // 将数据赋值data
       })
     }
